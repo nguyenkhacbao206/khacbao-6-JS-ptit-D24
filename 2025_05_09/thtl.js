7851339edf15fd841a4face2977f1132a1c6ff12
nguyenkhacbao206 <baonk.ptit@gmail.com>
2025-05-10 17:20:16 +0700

    main

---
+++ 2025_05_09/thtl.js
@@ -0,0 +1,19 @@
+var a = parseInt(prompt('Nhập số nguyên a: '));
+
+if(a==2){
+    document.write('Hôm nay là thứ hai');
+}else if(a==3){
+    document.write('Hôm nay là thứ ba');
+}else if(a==4){
+    document.write('Hôm nay là thứ tư');
+}else if(a==5){
+    document.write('Hôm nay là thứ năm');
+}else if(a==6){
+    document.write('Hôm nay là thứ sáu');
+}else if(a==7){
+    document.write('Hôm nay là thứ bảy');
+}else if(a==8){
+    document.write('Hôm nay là chủ nhật');
+}else{
+    document.write('Không hợp lệ');
+}
